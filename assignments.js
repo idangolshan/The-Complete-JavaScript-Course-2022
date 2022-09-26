10feb73cfb2d404f0b51f3fbd28ce1442df9ff80
Idan Golshan (HUBCAPP) <v-igolshan@microsoft.com>
2022-09-26 14:20:24 +0300

    part 1 - Done

---
+++ assignments.js
@@ -0,0 +1,105 @@
+/*
+const country = 'Israel';
+const continent = 'Asia';
+let population = 10;
+
+console.log(country, continent, population);
+
+const isIsland = false;
+const language = 'Hebrew';
+
+console.log(isIsland, population, country, language);
+
+//Basic operators
+
+let halfThePopulation = population / 2;
+
+console.log('halfThePopulation', halfThePopulation);
+
+console.log('population Plus One', population + 1);
+
+finlandPopulation = 6;
+averagePopulation = 32;
+
+population > finlandPopulation ? console.log('Israel has bigger population then Filand') : console.log('Filand has bigger population then Israel');
+
+console.log('population:', population)
+
+population > averagePopulation ? console.log('Israel has bigger population then the everage Population') : console.log('Israel population is smaller than the everage');
+
+const DESCRIPTION = 'Portugal is in Europe, and its 11 million people speak portuguese';
+*/
+
+// CODING CHALLANGE #1
+
+// let markHigherBMI;
+
+// const data1 = {
+//     markMass: 78,
+//     markHeight: 1.69,
+//     johnMass: 92,
+//     johnHeight: 1.95,
+// }
+
+// const data2 = {
+//     markMass: 95,
+//     markHeight: 1.88,
+//     johnMass: 85,
+//     johnHeight: 1.76,
+// }
+
+// const calculateBmi = (MarkMass, MarkHeight, johnMass, johnHeight) => {
+
+//     const markBmi = MarkMass / MarkHeight ** 2;
+
+//     const johnBmi = johnMass / johnHeight ** 2;
+
+//     markHigherBMI = markBmi > johnBmi;
+
+//     console.log(`Marks BMI is ${markBmi} and John BMI is ${johnBmi}`);
+
+//     console.log(`Mark's bmi (${markBmi}) higher then John's (${johnBmi})? ${markHigherBMI}`);
+// };
+
+// calculateBmi(data1.markMass, data1.markHeight, data1.johnMass, data1.johnHeight);
+
+// calculateBmi(data2.markMass, data2.markHeight, data2.johnMass, data2.johnHeight);
+
+
+//CODING CHALLANGE 2 ^^
+
+
+//CODING CHALLANGE 3
+
+// const avarageScores = (firstScore, secondScore, thirdScore) => (
+//     (firstScore + secondScore + thirdScore) / 3
+// );
+
+// const dolphinAvarageScore = avarageScores(96, 108, 89);
+// const koalasAvarageScore = avarageScores(88, 91, 110);
+
+
+// if (dolphinAvarageScore > koalasAvarageScore && dolphinAvarageScore >= 100) {
+//     console.log('Dolphins are the winners');
+// } else if (koalasAvarageScore > dolphinAvarageScore && koalasAvarageScore >= 100) {
+//     console.log('Koalas are the winners');
+// } else if (koalasAvarageScore === dolphinAvarageScore && koalasAvarageScore >= 100) {
+//     console.log(`It's a tie!`)
+// } else {
+//     console.log('No one wins the trophy :(');
+// }
+
+
+// Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+// Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+// Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+
+
+//CODING CHALLANGE 4
+
+// let bill = 430;
+
+// let tip = bill >= 50 && bill <= 300 ? tip = bill * 0.15 : tip = bill * 0.2
+
+// console.log(`“The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip} `);
+
